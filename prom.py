a0b5046a34cf63ab2d52b85202b4208592fc5389
Isaías Ortiz <44987358+Iza091@users.noreply.github.com>
2022-12-10 22:09:11 -0600

    Si jala

--- prom.py
+++ prom.py
@@ -8,6 +8,7 @@ PC1= float(input("Ingrese P1 : "))
 n3=PC1*0.40
 
 prom1 = (n1+n2+n3)
+promdiv=prom1/3
 print("EL PROMEDIO COMPUTO 1 ES : ", prom1)
 
 
@@ -21,6 +22,7 @@ PC2= float(input("Ingrese P1 : "))
 n3=PC2*0.40
 
 prom2 = (n1+n2+n3)
+promdv=prom2/3
 print("EL PROMEDIO COMPUTO 2 ES : ", prom2)
 
 
@@ -34,12 +36,21 @@ PC3= float(input("Ingrese P1 : "))
 n3=PC3*0.40
 
 prom3 = (n1+n2+n3)
+prmdv=prom3/3
 print("EL PROMEDIO COMPUTO 3 ES : ", prom3)
 
-c1=prom1*0.35
-c2=prom2*0.35
-c3=prom3*0.30
+c1=promdiv
+c2=promdv
+c3=prmdv
 
 print("TOTAL")
 promF=(c1+c2+c3)
-print("Nota final es: ",promF)
+pro= promF/3
+if promF >=6.0:
+    print("Felicidades, su nota es: ", promF)
+    9
+elif promF==5.9:
+ print("Paga diferido... ", promF) 
+else:
+ print("Ni modo negro, tu nota: ", promF)
+
